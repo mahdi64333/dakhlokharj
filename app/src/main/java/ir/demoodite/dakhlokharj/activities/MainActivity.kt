d925bcd39dba05e5a82154ec9c8443281b8eaf3a
Demoodite <m.jafari1379@gmail.com>
2023-07-11 05:01:59 +0330

    Reset to Ground zero

---
+++ app/src/main/java/ir/demoodite/dakhlokharj/activities/MainActivity.kt
@@ -0,0 +1,15 @@
+package ir.demoodite.dakhlokharj.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ir.demoodite.dakhlokharj.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
