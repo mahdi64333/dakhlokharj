80e4a1d59fbf97587d3f1263a0619ebda3c9ee9d
Demoodite <m.jafari1379@gmail.com>
2023-07-12 05:39:09 +0330

    Setup for base navigation

---
+++ app/src/main/java/ir/demoodite/dakhlokharj/fragments/HomeFragment.kt
@@ -0,0 +1,21 @@
+package ir.demoodite.dakhlokharj.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ir.demoodite.dakhlokharj.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
