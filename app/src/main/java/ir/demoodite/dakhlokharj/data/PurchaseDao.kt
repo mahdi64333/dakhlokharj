418663b72fb1414f251585c2080c72f11c9da212
Demoodite <m.jafari1379@gmail.com>
2023-07-14 19:04:49 +0330

    Ordered collection done

--- app/src/main/java/ir/demoodite/dakhlokharj/data/PurchaseDao.kt
+++ app/src/main/java/ir/demoodite/dakhlokharj/data/PurchaseDao.kt
@@ -26,9 +26,53 @@ interface PurchaseDao {
                 "FROM $purchasesTableName " +
                 "INNER JOIN $residentsTableName " +
                 "ON $purchaseBuyerId = $residentId " +
-                "WHERE $residentActive = 1"
+                "WHERE $residentActive = 1 " +
+                "ORDER BY $purchaseTime ASC"
     )
-    fun getAllDetailedPurchases(): Flow<List<DetailedPurchase>>
+    fun getAllDetailedPurchasesTimeAsc(): Flow<List<DetailedPurchase>>
+
+    @Query(
+        "SELECT $purchaseId, $purchaseProduct, $purchasePrice, " +
+                "$purchaseTime, $purchaseBuyerId, $residentName as buyerName " +
+                "FROM $purchasesTableName " +
+                "INNER JOIN $residentsTableName " +
+                "ON $purchaseBuyerId = $residentId " +
+                "WHERE $residentActive = 1 " +
+                "ORDER BY $purchaseTime DESC"
+    )
+    fun getAllDetailedPurchasesTimeDesc(): Flow<List<DetailedPurchase>>
+
+    @Query(
+        "SELECT $purchaseId, $purchaseProduct, $purchasePrice, " +
+                "$purchaseTime, $purchaseBuyerId, $residentName as buyerName " +
+                "FROM $purchasesTableName " +
+                "INNER JOIN $residentsTableName " +
+                "ON $purchaseBuyerId = $residentId " +
+                "WHERE $residentActive = 1 " +
+                "ORDER BY $purchasePrice ASC"
+    )
+    fun getAllDetailedPurchasesPriceAsc(): Flow<List<DetailedPurchase>>
+
+    @Query(
+        "SELECT $purchaseId, $purchaseProduct, $purchasePrice, " +
+                "$purchaseTime, $purchaseBuyerId, $residentName as buyerName " +
+                "FROM $purchasesTableName " +
+                "INNER JOIN $residentsTableName " +
+                "ON $purchaseBuyerId = $residentId " +
+                "WHERE $residentActive = 1 " +
+                "ORDER BY $purchasePrice DESC"
+    )
+    fun getAllDetailedPurchasesPriceDesc(): Flow<List<DetailedPurchase>>
+
+    fun getAllDetailedPurchases(orderColumn: String, order: String): Flow<List<DetailedPurchase>> {
+        return when {
+            orderColumn == purchaseTime && order == "ASC" -> getAllDetailedPurchasesTimeAsc()
+            orderColumn == purchaseTime && order == "DESC" -> getAllDetailedPurchasesTimeDesc()
+            orderColumn == purchasePrice && order == "ASC" -> getAllDetailedPurchasesPriceAsc()
+            orderColumn == purchasePrice && order == "DESC" -> getAllDetailedPurchasesPriceDesc()
+            else -> getAllDetailedPurchasesTimeDesc()
+        }
+    }
 
     @Insert
     suspend fun insert(purchase: Purchase): Long
