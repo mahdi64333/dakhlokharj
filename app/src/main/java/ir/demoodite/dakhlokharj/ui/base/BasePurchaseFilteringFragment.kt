3072a499e29ecd44f8ffbb19ba0b173b7fee815f
Demoodite <m.jafari1379@gmail.com>
2023-07-23 19:49:03 +0330

    Created new base fragment for filtering purchases fragments

---
+++ app/src/main/java/ir/demoodite/dakhlokharj/ui/base/BasePurchaseFilteringFragment.kt
@@ -0,0 +1,105 @@
+package ir.demoodite.dakhlokharj.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.view.isVisible
+import androidx.core.view.setPadding
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+import cn.pedant.SweetAlert.SweetAlertDialog
+import com.google.android.material.divider.MaterialDividerItemDecoration
+import ir.demoodite.dakhlokharj.R
+import ir.demoodite.dakhlokharj.data.room.DataRepository
+import ir.demoodite.dakhlokharj.data.room.models.DetailedPurchase
+import ir.demoodite.dakhlokharj.ui.components.filterpurchases.FilterPurchasesViewModel
+import ir.demoodite.dakhlokharj.ui.components.filterpurchases.filters.FilterBy
+import ir.demoodite.dakhlokharj.ui.components.home.PurchasesListAdapter
+import ir.demoodite.dakhlokharj.utils.LocaleHelper
+import ir.demoodite.dakhlokharj.utils.UiUtil
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import java.text.DecimalFormat
+import java.text.NumberFormat
+
+abstract class BasePurchaseFilteringFragment<T : ViewBinding>(
+    private val filterBy: FilterBy,
+    inflateMethod: (LayoutInflater, ViewGroup?, Boolean) -> T,
+) : BaseFragment<T>(inflateMethod) {
+    protected val viewModel: FilterPurchasesViewModel by activityViewModels()
+    private lateinit var decimalFormat: DecimalFormat
+
+    protected abstract val filteredPurchasesRecyclerView: RecyclerView
+    protected abstract val tvNoData: TextView
+    protected abstract val tvPurchasesPriceSum: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        decimalFormat =
+            NumberFormat.getInstance(LocaleHelper.getCurrentLocale(resources.configuration)) as DecimalFormat
+        startDataCollection()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupFilterInput()
+        setupPurchasesRecyclerView()
+    }
+
+    private fun startDataCollection() {
+        lifecycleScope.launch {
+            viewModel.getFilteredPurchasesStateFlow(filterBy).collectLatest {
+                repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    it?.let {
+                        updateFilteredPurchasesUi(it.first, it.second)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun updateFilteredPurchasesUi(purchases: List<DetailedPurchase>, priceSum: Long) {
+        val filteredPurchasesListAdapter =
+            filteredPurchasesRecyclerView.adapter as PurchasesListAdapter
+        filteredPurchasesListAdapter.submitList(purchases)
+        tvPurchasesPriceSum.text =
+            getString(R.string.purchases_sum, decimalFormat.format(priceSum))
+        tvNoData.isVisible = purchases.isEmpty()
+    }
+
+    protected abstract fun setupFilterInput()
+
+    private fun setupPurchasesRecyclerView() {
+        val adapter = PurchasesListAdapter(decimalFormat) {
+            UiUtil.setSweetAlertDialogNightMode(resources)
+            lifecycleScope.launch {
+                val consumers =
+                    DataRepository.getDatabase(requireContext()).consumerDao.getConsumerNamesOfPurchase(
+                        it.purchaseId
+                    ).first()
+                UiUtil.createConsumersSweetAlertDialog(requireContext(), consumers).apply {
+                    show()
+                    getButton(SweetAlertDialog.BUTTON_CONFIRM).setPadding(0)
+                }
+            }
+        }
+        filteredPurchasesRecyclerView.adapter = adapter
+        filteredPurchasesRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        filteredPurchasesRecyclerView.addItemDecoration(
+            MaterialDividerItemDecoration(
+                requireContext(), MaterialDividerItemDecoration.VERTICAL
+            ).apply {
+                isLastItemDecorated = false
+            })
+    }
+}
--- app/src/main/java/ir/demoodite/dakhlokharj/ui/components/filterpurchases/filters/FilterProductNameFragment.kt
+++ app/src/main/java/ir/demoodite/dakhlokharj/ui/components/filterpurchases/filters/FilterProductNameFragment.kt
@@ -1,78 +1,29 @@
 package ir.demoodite.dakhlokharj.ui.components.filterpurchases.filters
 
-import android.os.Bundle
-import android.view.View
-import androidx.core.view.isVisible
-import androidx.core.view.setPadding
-import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.recyclerview.widget.LinearLayoutManager
-import cn.pedant.SweetAlert.SweetAlertDialog
-import com.google.android.material.divider.MaterialDividerItemDecoration
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
 import ir.demoodite.dakhlokharj.R
-import ir.demoodite.dakhlokharj.data.room.DataRepository
-import ir.demoodite.dakhlokharj.data.room.models.DetailedPurchase
 import ir.demoodite.dakhlokharj.databinding.FragmentFilterProductNameBinding
-import ir.demoodite.dakhlokharj.ui.base.BaseFragment
-import ir.demoodite.dakhlokharj.ui.components.filterpurchases.FilterPurchasesViewModel
-import ir.demoodite.dakhlokharj.ui.components.home.PurchasesListAdapter
-import ir.demoodite.dakhlokharj.utils.LocaleHelper
+import ir.demoodite.dakhlokharj.ui.base.BasePurchaseFilteringFragment
 import ir.demoodite.dakhlokharj.utils.UiUtil
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
-import java.text.DecimalFormat
-import java.text.NumberFormat
 
 @AndroidEntryPoint
 class FilterProductNameFragment :
-    BaseFragment<FragmentFilterProductNameBinding>(FragmentFilterProductNameBinding::inflate) {
-    private val viewModel: FilterPurchasesViewModel by activityViewModels()
-    private lateinit var decimalFormat: DecimalFormat
+    BasePurchaseFilteringFragment<FragmentFilterProductNameBinding>(
+        FilterBy.PRODUCT_NAME, FragmentFilterProductNameBinding::inflate
+    ) {
+    override val filteredPurchasesRecyclerView: RecyclerView
+        get() = binding.rvPurchasesFiltered
+    override val tvNoData: TextView
+        get() = binding.tvFilteredNoData
+    override val tvPurchasesPriceSum: TextView
+        get() = binding.tvPurchasesSum
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun setupFilterInput() {
+        val productName = binding.textInputEditTextFilter.text.toString().trim()
 
-        decimalFormat =
-            NumberFormat.getInstance(LocaleHelper.getCurrentLocale(resources.configuration)) as DecimalFormat
-        startDataCollection()
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        setupFilterInput()
-        setupPurchasesRecyclerView()
-    }
-
-    private fun startDataCollection() {
-        lifecycleScope.launch {
-            viewModel.getFilteredPurchasesStateFlow(FilterBy.PRODUCT_NAME).collectLatest {
-                repeatOnLifecycle(Lifecycle.State.STARTED) {
-                    it?.let {
-                        updateFilteredPurchasesUi(it.first, it.second)
-                    }
-                }
-            }
-        }
-    }
-
-    private fun updateFilteredPurchasesUi(purchases: List<DetailedPurchase>, priceSum: Long) {
-        val filteredPurchasesListAdapter =
-            binding.rvPurchasesFiltered.adapter as PurchasesListAdapter
-        filteredPurchasesListAdapter.submitList(purchases)
-        binding.tvPurchasesSum.text =
-            getString(R.string.purchases_sum, decimalFormat.format(priceSum))
-        binding.tvFilteredNoData.isVisible = purchases.isEmpty()
-    }
-
-    private fun setupFilterInput() {
         binding.textInputLayoutFilter.setEndIconOnClickListener {
-            val productName = binding.textInputEditTextFilter.text.toString().trim()
-
             if (productName.isEmpty()) {
                 binding.textInputLayoutFilter.error = getString(R.string.its_empty)
                 UiUtil.removeErrorOnType(binding.textInputEditTextFilter)
@@ -80,29 +31,5 @@ class FilterProductNameFragment :
                 viewModel.filterByProductName(productName)
             }
         }
-    }
-
-    private fun setupPurchasesRecyclerView() {
-        val adapter = PurchasesListAdapter(decimalFormat) {
-            UiUtil.setSweetAlertDialogNightMode(resources)
-            lifecycleScope.launch {
-                val consumers =
-                    DataRepository.getDatabase(requireContext()).consumerDao.getConsumerNamesOfPurchase(
-                        it.purchaseId
-                    ).first()
-                UiUtil.createConsumersSweetAlertDialog(requireContext(), consumers).apply {
-                    show()
-                    getButton(SweetAlertDialog.BUTTON_CONFIRM).setPadding(0)
-                }
-            }
-        }
-        binding.rvPurchasesFiltered.adapter = adapter
-        binding.rvPurchasesFiltered.layoutManager = LinearLayoutManager(requireContext())
-        binding.rvPurchasesFiltered.addItemDecoration(
-            MaterialDividerItemDecoration(
-                requireContext(), MaterialDividerItemDecoration.VERTICAL
-            ).apply {
-                isLastItemDecorated = false
-            })
     }
 }
