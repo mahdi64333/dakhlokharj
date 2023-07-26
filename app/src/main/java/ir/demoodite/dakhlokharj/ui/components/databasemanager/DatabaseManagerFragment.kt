fe8c629bbe041dfd8dd329a19fd3a730b24f1466
Demoodite <m.jafari1379@gmail.com>
2023-07-26 21:01:35 +0330

    Database archiving ui done

--- app/src/main/java/ir/demoodite/dakhlokharj/ui/components/databasemanager/DatabaseManagerFragment.kt
+++ app/src/main/java/ir/demoodite/dakhlokharj/ui/components/databasemanager/DatabaseManagerFragment.kt
@@ -1,17 +1,24 @@
 package ir.demoodite.dakhlokharj.ui.components.databasemanager
 
 import android.os.Bundle
+import android.text.InputFilter
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import dagger.hilt.android.AndroidEntryPoint
 import ir.demoodite.dakhlokharj.R
 import ir.demoodite.dakhlokharj.databinding.FragmentDatabaseManagerBinding
+import ir.demoodite.dakhlokharj.databinding.ViewDialogDatabaseAliasBinding
 import ir.demoodite.dakhlokharj.ui.base.BaseFragment
+import ir.demoodite.dakhlokharj.utils.UiUtil
 
+@AndroidEntryPoint
 class DatabaseManagerFragment :
     BaseFragment<FragmentDatabaseManagerBinding>(FragmentDatabaseManagerBinding::inflate) {
     private val viewModel: DatabaseManagerViewModel by viewModels()
@@ -31,7 +38,7 @@ class DatabaseManagerFragment :
             override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
                 return when (menuItem.itemId) {
                     R.id.action_archive_current_database -> {
-                        archiveCurrentDatabase()
+                        showArchiveCurrentDatabaseDialog()
                         true
                     }
                     R.id.action_import_database_from_file -> {
@@ -44,7 +51,49 @@ class DatabaseManagerFragment :
         }, viewLifecycleOwner, Lifecycle.State.STARTED)
     }
 
-    private fun archiveCurrentDatabase() {
+    private fun showArchiveCurrentDatabaseDialog() {
+        val databaseAliasDialogBinding =
+            ViewDialogDatabaseAliasBinding.inflate(layoutInflater, null, false)
+        databaseAliasDialogBinding.textInputEditTextDatabaseAlias.filters = arrayOf(
+            InputFilter.LengthFilter(24),
+            FilenameInputFilter()
+        )
+
+        MaterialAlertDialogBuilder(
+            requireContext()
+        ).setTitle(getString(R.string.database_alias))
+            .setMessage(getString(R.string.please_enter_database_alias))
+            .setView(databaseAliasDialogBinding.root)
+            .setPositiveButton(getString(R.string.confirm), null)
+            .setNegativeButton(getString(R.string.cancel), null)
+            .show().apply {
+                getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+                    val alias = validateAndGetDatabaseAliasDialog(databaseAliasDialogBinding)
+                    if (alias != null) {
+                        archiveCurrentDatabase(alias)
+                        dismiss()
+                    }
+                }
+            }
+    }
+
+    private fun validateAndGetDatabaseAliasDialog(
+        databaseAliasBinding: ViewDialogDatabaseAliasBinding,
+    ): String? {
+        var errorFlag = false
+
+        val aliasText = databaseAliasBinding.textInputEditTextDatabaseAlias.text.toString().trim()
+
+        if (aliasText.isEmpty()) {
+            databaseAliasBinding.textInputLayoutDatabaseAlias.error = getString(R.string.its_empty)
+            UiUtil.removeErrorOnTextChange(databaseAliasBinding.textInputEditTextDatabaseAlias)
+            errorFlag = true
+        }
+
+        return if (errorFlag) null else aliasText
+    }
+
+    private fun archiveCurrentDatabase(alias: String) {
         TODO()
     }
 
---
+++ app/src/main/java/ir/demoodite/dakhlokharj/ui/components/databasemanager/FilenameInputFilter.kt
@@ -0,0 +1,20 @@
+package ir.demoodite.dakhlokharj.ui.components.databasemanager
+
+import android.text.InputFilter
+import android.text.Spanned
+
+class FilenameInputFilter : InputFilter {
+    val reservedChars = "|\\?*<\":>+[]/'."
+
+
+    override fun filter(
+        source: CharSequence?,
+        start: Int,
+        end: Int,
+        dest: Spanned?,
+        dstart: Int,
+        dend: Int,
+    ): CharSequence {
+        return source?.filter { !reservedChars.contains(it) } ?: ""
+    }
+}
