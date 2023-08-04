a317ad59b43e42b471e1da696bc3802f46118c55
Demoodite <m.jafari1379@gmail.com>
2023-08-04 17:40:22 +0330

    Added: Ui feedback system for MainActivity
    Now it is much easier to send ui events from ViewModels to ui

--- app/src/main/java/ir/demoodite/dakhlokharj/ui/components/mainactivity/MainActivity.kt
+++ app/src/main/java/ir/demoodite/dakhlokharj/ui/components/mainactivity/MainActivity.kt
@@ -3,21 +3,32 @@ package ir.demoodite.dakhlokharj.ui.components.mainactivity
 import android.os.Bundle
 import android.view.View
 import androidx.activity.OnBackPressedCallback
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isGone
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
+import cn.pedant.SweetAlert.SweetAlertDialog
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import ir.demoodite.dakhlokharj.R
 import ir.demoodite.dakhlokharj.data.settings.SettingsDataStore
 import ir.demoodite.dakhlokharj.databinding.ActivityMainBinding
 import ir.demoodite.dakhlokharj.utils.LocaleHelper
+import ir.demoodite.dakhlokharj.utils.UiUtil
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -38,6 +49,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupLanguageSettingDataCollection()
+        listenForUiFeedbackRequests()
         setupNavigation()
         handleDrawerBackPressed()
     }
@@ -50,6 +62,39 @@ class MainActivity : AppCompatActivity() {
         lifecycleScope.launch {
             settingsDataStore.getLanguageFlow().collectLatest {
                 LocaleHelper.applicationLanguageCode = it
+            }
+        }
+    }
+
+    private fun listenForUiFeedbackRequests() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                getUiFeedbackReceiver().collectLatest { (type, messageResId) ->
+                    when (type) {
+                        FeedbackType.ERROR_DIALOG -> {
+                            UiUtil.setSweetAlertDialogNightMode(resources)
+                            SweetAlertDialog(
+                                this@MainActivity, SweetAlertDialog.ERROR_TYPE
+                            ).apply {
+                                titleText = getString(R.string.an_error_has_occurred)
+                                contentText = getString(messageResId)
+                                confirmText = getString(R.string.confirm)
+                                setConfirmClickListener { dismiss() }
+                                show()
+                                getButton(SweetAlertDialog.BUTTON_CONFIRM).apply {
+                                    UiUtil.fixSweetAlertDialogButtons(this)
+                                }
+                            }
+                        }
+                        FeedbackType.SNACKBAR -> {
+                            Snackbar.make(
+                                binding.root,
+                                messageResId,
+                                Snackbar.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+                }
             }
         }
     }
@@ -107,5 +152,44 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    companion object UiFeedbackChannel {
+        @Volatile
+        private var INSTANCE: Channel<Pair<FeedbackType, Int>>? = null
+
+        private fun getUiFeedbackReceiver(): Flow<Pair<FeedbackType, Int>> {
+            return INSTANCE?.receiveAsFlow() ?: synchronized(this) {
+                if (INSTANCE == null) {
+                    INSTANCE = Channel()
+                }
+                INSTANCE!!.receiveAsFlow()
+            }
+        }
+
+        private fun getUiFeedbackChannel(): Channel<Pair<FeedbackType, Int>> {
+            return INSTANCE ?: synchronized(this) {
+                if (INSTANCE == null) {
+                    INSTANCE = Channel()
+                }
+                INSTANCE!!
+            }
+        }
+
+        suspend fun sendError(@StringRes errorRes: Int) {
+            withContext(Dispatchers.Main) {
+                getUiFeedbackChannel().send(Pair(FeedbackType.ERROR_DIALOG, errorRes))
+            }
+        }
+
+        suspend fun sendMessage(@StringRes messageRes: Int) {
+            withContext(Dispatchers.Main) {
+                getUiFeedbackChannel().send(Pair(FeedbackType.SNACKBAR, messageRes))
+            }
+        }
+
+        enum class FeedbackType {
+            ERROR_DIALOG, SNACKBAR,
+        }
     }
 }
